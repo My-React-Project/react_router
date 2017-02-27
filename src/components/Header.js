97722945448d75dde7634584e150e93adbd13750
choijaewoong <jwchoi0129@gmail.com>
2017-02-27 19:37:02 +0900

    메뉴 Active 효과 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,56 @@ import './Header.css';
 const MenuItem = ({active, children, to}) => (
   /*
     Link : 페이지를 새로 로딩할 필요 없이 지정된 Component를 보여줄때 사용
+    여러개의 Class를 추가할 경우 ` `를 이용하여 추가 (ES6 문법)
   */
-  <Link to={to} className="menu-item">
+  <Link to={to} className={`menu-item ${active ? 'active' : ''}`}>
     {children}
   </Link>
 )
 
-const Header = () => {
+const Header = (props, context) => {
+  const { router } = context;
   return (
     <div>
       <div className="logo">
         jaewoong
       </div>
       <div className="menu">
-        <MenuItem to={'/'}>Home</MenuItem>
-        <MenuItem to={'/about'}>About</MenuItem>
-        <MenuItem to={'/posts'}>Posts</MenuItem>
+        {/*
+             active : 현재 브라우저의 경로와 주어진 경로와 매칭이 되는지 확인 하는 props
+        */}
+        <MenuItem to={'/'} active={router.isActive('/', true)}>Home</MenuItem>
+        <MenuItem to={'/about'} active={router.isActive('/about', true)}>About</MenuItem>
+        <MenuItem to={'/posts'} active={router.isActive('/posts', true)}>Posts</MenuItem>
       </div>
     </div>
   );
 };
+// Class를 이용한 Header Component 정의
+// class Header extends React.Component {
+//     render() {
+//         let router = this.context.router;
+//         return (
+//             <div>
+//               <div className="logo">
+//                 jaewoong
+//               </div>
+//               <div className="menu">
+//                 {/*
+//                      active : 현재 브라우저의 경로와 주어진 경로와 매칭이 되는지 확인 하는 props
+//                 */}
+//                 <MenuItem to={'/'} active={router.isActive('/', true)}>Home</MenuItem>
+//                 <MenuItem to={'/about'} active={router.isActive('/about', true)}>About</MenuItem>
+//                 <MenuItem to={'/posts'} active={router.isActive('/posts', true)}>Posts</MenuItem>
+//               </div>
+//             </div>
+//         );
+//     }
+// }
+
+// 컴포넌트의 context 객체의 router 에 접근을 하기 위한 설정
+Header.contextTypes = {
+  router : React.PropTypes.object
+}
 
 export default Header;
